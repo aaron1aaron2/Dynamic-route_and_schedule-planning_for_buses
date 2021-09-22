3ca8896614d7e5d59ed0144f59a64d44f7f5ff08
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2021-09-22 10:15:56 +0800

    Update  google-2point-distance-crawler  multipath

--- data_processing/sub_project/googlemap_2pointdistance_crawler.py
+++ data_processing/sub_project/googlemap_2pointdistance_crawler.py
@@ -19,6 +19,7 @@ from multiprocessing import Pool
 from bs4 import BeautifulSoup
 from webdriver_manager.chrome import ChromeDriverManager
 
+from IPython import embed
 ### Tor 切換IP與多開 -> https://hardliver.blogspot.com/2018/03/tor-tor-client.htmls
 
 class crawler:
@@ -138,7 +139,8 @@ class crawler:
         for v, name in tqdm.tqdm(enumerate(df)):
             minute = []
             count = 0
-            browser.get('https://www.google.com.tw/maps/dir/' + str(name) + '/data=!3m1!4b1!4m2!4m1!3e0') #最後一碼為0汽車、2走路
+            url = 'https://www.google.com.tw/maps/dir/' + str(name) + '/data=!3m1!4b1!4m2!4m1!3e0'
+            browser.get(url) #最後一碼為0汽車、2走路
 
             # 最多執行50次，沒有的話則輸出nan，並切換IP
             while not minute:
@@ -150,37 +152,50 @@ class crawler:
                         with Controller.from_port(port=int(proxy[-4:]) + 1) as controller:
                             controller.authenticate()
                             controller.signal(Signal.NEWNYM)
+
                     except stem.SocketError as exc:
                         print("Unable to connect to tor on port 9051: %s" % exc)
 
                     browser.close()
                     browser = self.chrome(proxy)
-                    browser.get('https://www.google.com.tw/maps/dir/' + str(name) + '/data=!3m1!4b1!4m2!4m1!3e0') #最後一碼為0汽車、2走路
+                    browser.get(url) #最後一碼為0汽車、2走路
 
                 if count == 20:
                     print(f'[Error] {name} not available')
                     break
                 _ = browser.find_element_by_xpath('//*[@id="section-directions-trip-travel-mode-0"]') #等到找到這個在往下
+                
                 soup = BeautifulSoup(browser.page_source, "html.parser")
+
+                routes = soup.find_all("h1", class_ = re.compile(".+trip.+"))
+                routes = [i.text for i in routes if i.text.strip()!='']
+
                 minute = soup.find_all("div", class_ = re.compile(".+trip.+"))
                 minute = [(i.text, re.findall(r'\d+\D{,3}', i.text)) for i in minute if (len(i.text)<40) & (i.text.strip()!='')]
-                minute = [[text, 'distance', select_ls] if (text.find('公里') !=-1 | text.find('公尺')) else (
+                minute = [[text, 'distance', [''.join(select_ls)]] if (text.find('公里') !=-1 | text.find('公尺')) else (
                             [text, 'trip duration', select_ls] if (text.find('預估行車時間') != -1) else (
                             [text, 'trip duration(Smooth)', select_ls] if (text.find('交通順暢') != -1) else
                             [text, 'unknow', select_ls]
                             )) for text, select_ls in minute if len(select_ls)!=0]
 
+                tmp = []
+                rt_idx = -1
+                for text, item_type, initial_value in minute:
+                    if item_type == 'trip duration':
+                        rt_idx += 1
+                    if rt_idx >= len(routes):
+                        tmp.append(['unknown', text, item_type, initial_value, url])
+                    else:
+                        tmp.append([routes[rt_idx], text, item_type, initial_value, url])
+
             # 將每筆資料同步輸出
-            if len(minute) != 0:
+            if len(tmp) != 0:
                 with open(self.output_path, 'a', newline='', encoding='utf-8') as csvfile:
                     time.sleep(0.5)
                     writer = csv.writer(csvfile)
-                    for i in minute:
+                    for i in tmp:
                         combine = [name] + i
                         writer.writerow(combine)
-
-            # if v%50 == 0:
-            #     print(f'[{v}/{df.shape[0]}]')
 
         # 關閉瀏覽器
         browser.close()
@@ -220,9 +235,9 @@ class crawler:
                 self.pool_handler(arg_list)
 
 if __name__ == "__main__":
-    core = 2 # 多開的數量
-    data_path = './output/1_distance_table_half.csv' # 原始檔的檔案名稱
-    tor_path = 'tor-win32-0.4.3.6/Tor/tor.exe'
+    core = 1 # 多開的數量
+    data_path = '../data/proccess/3_driving_distance(time)/test/distance_table_half.csv' # 原始檔的檔案名稱
+    tor_path = '../tool/tor-win32-0.4.3.6/Tor/tor.exe'
     tor_confs_path = 'tor_confs'
 
     googlecrawler = crawler(
@@ -231,4 +246,4 @@ if __name__ == "__main__":
         tor_confs_path=tor_confs_path, 
         core=core
         )
-    googlecrawler.run(debug=True)
+    googlecrawler.run()
