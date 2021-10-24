6378aca0ce7ca2f2d3eea50a6316b29a958b9752
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2021-10-24 15:34:18 +0800

    Update cache

--- DSRP/utils.py
+++ DSRP/utils.py
@@ -5,7 +5,9 @@ Contact: aaron1aaron2@gmail.com
 GitHub: https://github.com/aaron1aaron2
 Create Date:  20210907
 """
+import os
 import argparse
+import pandas as pd
 
 def str2bool(v):
     if isinstance(v, bool):
@@ -16,3 +18,20 @@ def str2bool(v):
         return False
     else:
         raise argparse.ArgumentTypeError('Boolean value expected.')
+
+def cache(data, name, switch):
+    if switch:
+        data.to_pickle(f'output/.cache/{name}.pkl')
+
+def read_cache(name, switch):
+    path = f'output/.cache/{name}.pkl'
+    if switch:
+        try:
+            df = pd.read_pickle(path)
+            print(f'load cache at {path}')
+            return False, df
+        except:
+            print(f'no cache detect at {path}')
+            return True, None
+    else:
+        return True, None
